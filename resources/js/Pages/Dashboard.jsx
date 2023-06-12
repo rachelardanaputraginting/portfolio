4f3c8327d38cae2c61c996b1fcd17c5e4cc04ffc
rachelardanaputraginting <rachelardanaputraginting@gmail.com>
2023-06-12 14:19:08 +0700

    Memperbaiki dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,22 +1,38 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
+import React from 'react';
 import { Head } from '@inertiajs/react';
+import Container from '@/Components/Container';
+import App from '@/Layouts/App';
+import Header from '@/Components/Header';
+import Navigation from '@/Components/Navigation';
+import CardHorizontal from '@/Components/CardHorizontal';
+import Title from '@/Components/Title';
 
 export default function Dashboard(props) {
     return (
-        <AuthenticatedLayout
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>}
-        >
-            <Head title="Dashboard" />
-
-            <div className="py-12">
-                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
-                    </div>
+        <>
+            <Head title="Dashbboard" />
+            <Container>
+                <div className="grid grid-cols-2 md:grid-cols-7 gap-4">
+                    <Navigation href={`/dashboard`}>Dashboard</Navigation>
+                    <Navigation href={`/admin/skills`}>Skills</Navigation>
+                    <Navigation href={`/admin/products`}>Products</Navigation>
+                    <Navigation href={`/admin/educations`}>Educations</Navigation>
+                    <Navigation href={`/admin/experiences`}>Experiences</Navigation>
+                    <Navigation href={`/admin/achievements`}>Achievements</Navigation>
                 </div>
-            </div>
-        </AuthenticatedLayout>
+            </Container>
+
+            <Container>
+                <Title title="Admin - Dashboard" />
+            </Container>
+
+            <Container>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+
+                </div>
+            </Container >
+        </>
     );
 }
+
+Dashboard.layout = page => <App children={page} />
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AchievementController;
 use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EducationController;
 use App\Http\Controllers\ExperiencesController;
 use App\Http\Controllers\HomeController;
@@ -18,11 +19,12 @@ Route::resource('achievements', AchievementController::class);
 Route::resource('educations', EducationController::class);
 Route::resource('experiences', ExperiencesController::class);
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -36,21 +38,6 @@ Route::middleware('auth')->group(function () {
         Route::get('/admin/products/{product:slug}/edit', 'edit')->name('admin.products.edit');
         Route::put('/admin/products/{product}', 'update')->name('admin.products.update');
     });
-
-    // // Educations
-    // Route::controller(AdminEducationController::class)->group(function () {
-    //     Route::get('admin/educations', 'index')->name('admin.educations.index');
-    //     Route::get('admin/educations/create', 'create')->name('admin.educations.create');
-    //     Route::delete('admin/educations/{product:slug}', 'destroy')->name('admin.educations.destroy');
-    //     Route::post('admin/educations', 'store')->name('admin.educations.store');
-    //     Route::get('admin/educations/{product:slug}/edit', 'edit')->name('admin.educations.edit');
-    //     Route::put('admin/educations/{product:slug}', 'update')->name('admin.educations.update');
-    // });
-
-    // Route::resource('admin/skills', AdminSkillController::class);
-    // Route::resource('admin/educations', AdminEducationController::class);
-    // Route::resource('admin/achievements', AdminAchievementController::class);
-    // Route::resource('admin/products', AdminProductController::class);
 });
 
 require __DIR__ . '/auth.php';
